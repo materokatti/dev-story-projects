167cdbd19d7e35d2fec069671641b5aa447a7b73
Doyun <materokatti@gmail.com>
2024-10-17 09:04:29 +0900

    style : change hover style and add grid gap

--- app/page.tsx
+++ app/page.tsx
@@ -35,10 +35,10 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid gap-5 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left ">
         <a
           href="/pong"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:dark:border-neutral-700"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
@@ -53,7 +53,7 @@ export default function Home() {
         </a>
         <a
           href="/particlesParty"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:dark:border-neutral-700"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
