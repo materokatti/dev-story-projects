9e08ec3304867fa4b949984539ab18a2309d0754
Doyun <materokatti@gmail.com>
2024-10-29 08:14:42 +0900

    feat : add slice number input

--- app/circleArea/page.tsx
+++ app/circleArea/page.tsx
@@ -34,6 +34,11 @@ const CircleAreaPage: React.FC = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center bg-black">
             <canvas ref={canvasRef} />
+            <input
+                type="number"
+                placeholder="Enter number"
+                className=" absolute mt-4 px-4 py-2 rounded-lg border-4 border-black focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+            />
         </div>
     );
 };
