5bfc13e67a170bbbc4afc0a15e0724a1d434ab74
Doyun <materokatti@gmail.com>
2024-11-04 22:01:13 +0900

    feat : add buymeacoffee again

--- app/circleArea/page.tsx
+++ app/circleArea/page.tsx
@@ -118,6 +118,13 @@ const CircleAreaPage: React.FC = () => {
             Line Color to {strokeColor === "black" ? "White" : "Black"}
           </button>
         </div>
+        <div>
+          <div>
+            <a href='https://www.buymeacoffee.com/materokattl'>
+              <img src='https://img.buymeacoffee.com/button-api/?text=Buy me a Onigiri&emoji=🍙&slug=materokattl&button_colour=FFDD00&font_colour=000000&font_family=Comic&outline_colour=000000&coffee_colour=ffffff' />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
