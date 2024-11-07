6c90299bcf9b6568e38a7a77784842f624b43abf
Doyun <materokatti@gmail.com>
2024-11-07 20:31:19 +0900

    feat : change particleCount

--- app/particlesParty/page.tsx
+++ app/particlesParty/page.tsx
@@ -1,95 +1,97 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 
 const ParticlesPartyPage: React.FC = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      if (canvas) {
-        const updateCanvasSize = () => {
-          canvas.width = window.innerWidth;
-          canvas.height = window.innerHeight;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const updateCanvasSize = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
       };
 
-      window.addEventListener('resize', updateCanvasSize);
+      window.addEventListener("resize", updateCanvasSize);
 
       updateCanvasSize();
-      
-        const ctx = canvas.getContext("2d");
-        const particles: { x: number; y: number; vx: number; vy: number }[] = [];
-        const particleCount = 700;
-  
-        const createParticles = () => {
-          for (let i = 0; i < particleCount; i++) {
-            const angle = Math.random() * Math.PI * 2;
-            const speed = Math.random() * 2 + 1;
-            particles.push({
+
+      const ctx = canvas.getContext("2d");
+      const particles: {x: number; y: number; vx: number; vy: number}[] = [];
+      const particleCount = 1000;
+
+      const createParticles = () => {
+        for (let i = 0; i < particleCount; i++) {
+          const angle = Math.random() * Math.PI * 2;
+          const speed = Math.random() * 2 + 1;
+          particles.push({
+            x: canvas.width / 2,
+            y: canvas.height / 2,
+            vx: Math.cos(angle) * speed,
+            vy: Math.sin(angle) * speed,
+          });
+        }
+      };
+
+      const updateParticles = () => {
+        ctx?.clearRect(0, 0, canvas.width, canvas.height);
+
+        particles.forEach((particle, index) => {
+          particle.x += particle.vx;
+          particle.y += particle.vy;
+
+          ctx!.beginPath();
+          ctx!.arc(particle.x, particle.y, 1.5, 0, Math.PI * 2);
+          ctx!.fillStyle = "#F8C44C";
+          ctx!.fill();
+
+          if (
+            particle.x < 0 ||
+            particle.x > canvas.width ||
+            particle.y < 0 ||
+            particle.y > canvas.height
+          ) {
+            particles[index] = {
               x: canvas.width / 2,
               y: canvas.height / 2,
-              vx: Math.cos(angle) * speed,
-              vy: Math.sin(angle) * speed,
-            });
+              vx:
+                Math.cos(Math.random() * Math.PI * 2) * (Math.random() * 2 + 1),
+              vy:
+                Math.sin(Math.random() * Math.PI * 2) * (Math.random() * 2 + 1),
+            };
           }
-        };
-  
-        const updateParticles = () => {
-          ctx?.clearRect(0, 0, canvas.width, canvas.height);
-  
-          particles.forEach((particle, index) => {
-            particle.x += particle.vx;
-            particle.y += particle.vy;
-  
-            ctx!.beginPath();
-            ctx!.arc(particle.x, particle.y, 1.5, 0, Math.PI * 2);
-            ctx!.fillStyle = "#F8C44C";
-            ctx!.fill();
-  
-            if (
-              particle.x < 0 ||
-              particle.x > canvas.width ||
-              particle.y < 0 ||
-              particle.y > canvas.height
-            ) {
-              particles[index] = {
-                x: canvas.width / 2,
-                y: canvas.height / 2,
-                vx: Math.cos(Math.random() * Math.PI * 2) * (Math.random() * 2 + 1),
-                vy: Math.sin(Math.random() * Math.PI * 2) * (Math.random() * 2 + 1),
-              };
-            }
-          });
-        };
-        const animate = () => {
-          updateParticles();
-          requestAnimationFrame(animate);
-        };
-  
-        if (ctx) {
-          createParticles();
-          animate();
-        }
-        return () => {
-          window.removeEventListener('resize', updateCanvasSize);
-        }
+        });
+      };
+      const animate = () => {
+        updateParticles();
+        requestAnimationFrame(animate);
+      };
+
+      if (ctx) {
+        createParticles();
+        animate();
       }
-    }, []);
-  
-    return (
-      <div
-        style={{
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          background: "black",
-        }}
-      >
-        <canvas ref={canvasRef} />
-      </div>
-    );
-  };
+      return () => {
+        window.removeEventListener("resize", updateCanvasSize);
+      };
+    }
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "black",
+      }}
+    >
+      <canvas ref={canvasRef} />
+    </div>
+  );
+};
 
 export default ParticlesPartyPage;
